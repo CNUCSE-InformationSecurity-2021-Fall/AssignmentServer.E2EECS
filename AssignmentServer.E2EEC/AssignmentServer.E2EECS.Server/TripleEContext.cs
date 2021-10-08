bdd54ea8472b79665d5e4288eeae4ccc9f77f4f5
Kangjun Heo == P.Knowledge <knowledge@o.cnu.ac.kr>
2021-10-08 17:35:07 +0900

    TripleEContext for application level of stack

---
+++ AssignmentServer.E2EEC/AssignmentServer.E2EECS.Server/TripleEContext.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentServer.E2EECS.Server
+{
+    public class TripleEContext
+    {
+        public TripleEMethods Method { get; set; }
+        public Dictionary<string, string> Headers { get; set; }
+        public string Body { get; set; }
+
+        public byte[] Serialize()
+        {
+            var payloadBuilder = new StringBuilder("3EPROTO ");
+
+            payloadBuilder.AppendFormat("{0}\n", Method.ToString());
+
+            Headers.Keys.ToList()
+                   .ForEach(key =>
+                        payloadBuilder.AppendFormat("{0}:{1}\n", key, Headers[key])
+                   );
+
+            payloadBuilder.AppendLine();
+            payloadBuilder.Append(Body);
+
+            return Encoding.UTF8.GetBytes(payloadBuilder.ToString());
+        }
+
+        public static TripleEContext ResponseContext()
+        {
+            return new()
+            {
+                Method = TripleEMethods.UNKNOWN,
+                Headers = new()
+                {
+                    { "Timestamp", DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss") }
+                },
+                Body = null
+            };
+        }
+
+        public static TripleEContext ErrorContext(string message = null)
+        {
+            var context = ResponseContext();
+
+            context.Method = TripleEMethods.DENY;
+            context.Body = message;
+
+            return context;
+        }
+    }
+}
