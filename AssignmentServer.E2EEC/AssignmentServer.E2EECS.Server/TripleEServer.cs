5531a69e4c04b97580e9f30125c1ee329350bed3
Kangjun Heo == P.Knowledge <knowledge@o.cnu.ac.kr>
2021-10-11 03:23:30 +0900

    complete remaining features

--- AssignmentServer.E2EEC/AssignmentServer.E2EECS.Server/TripleEServer.cs
+++ AssignmentServer.E2EEC/AssignmentServer.E2EECS.Server/TripleEServer.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace AssignmentServer.E2EECS.Server
@@ -11,17 +13,28 @@ namespace AssignmentServer.E2EECS.Server
     public class TripleEServer
     {
         readonly InternalSocket isck;
-        readonly Dictionary<string, TripleEContext> sessions;
+        readonly Dictionary<string, InternalSocketContext> sessions;
+
+        readonly List<TripleEMethods> RelayResponseMethods = new() {
+                TripleEMethods.KEYXCHG,
+                TripleEMethods.KEYXCHGRST,
+                TripleEMethods.KEYXCHGOK, 
+                TripleEMethods.KEYXCHGFAIL,
+                TripleEMethods.MSGSENDOK,
+                TripleEMethods.MSGSENDFAIL
+        };
 
         public TripleEServer()
         {
-            isck = new InternalSocket(8080);
+            isck = new InternalSocket(8200);
             isck.OnDataReceived = InternalSocketReceived;
 
-            sessions = new Dictionary<string, TripleEContext>();
+            sessions = new Dictionary<string, InternalSocketContext>();
+
+            isck.Ignite();
         }
 
-        public bool SessionAdd(string name, TripleEContext context)
+        public bool SessionAdd(string name, InternalSocketContext context)
         {
             if (sessions.ContainsKey(name))
                 return false;
@@ -30,7 +43,7 @@ namespace AssignmentServer.E2EECS.Server
             return true;
         }
 
-        public TripleEContext SessionGet(string name)
+        public InternalSocketContext SessionGet(string name)
         {
             return sessions.GetValueOrDefault(name);
         }
@@ -43,17 +56,91 @@ namespace AssignmentServer.E2EECS.Server
             return sessions.Remove(name);
         }
 
+        public void SessionValidate(TripleEContext context, string credential)
+        {
+            var session = SessionGet(credential);
+
+            if (session is null)
+                throw new Exception("session not exists");
+
+            if (session.AcceptedSocket.RemoteEndPoint != context.SocketContext.AcceptedSocket.RemoteEndPoint)
+                throw new Exception("session and connection does not match");
+
+        }
+
         private byte[] DispatchMethod(TripleEContext context)
         {
-            var serverType = typeof(TripleEServer);
-            var method = serverType.GetMethod(context.Method.ToString());
+            TripleEContext result = null;
 
-            if (method is null)
-                throw new Exception("Invalid Method");
-            
-            var result = method.Invoke(this, new object[] { context }) as TripleEContext;
+            if (RelayResponseMethods.Contains(context.Method)) 
+            {
+                result = this.Method_RELAY(context);
+            }
+            else
+            {
+                var serverType = typeof(TripleEServerExtensions);
+                var method = serverType.GetMethod($"Method_{context.Method}");
 
-            return result.Serialize();
+                if (method is null)
+                    throw new Exception("Invalid Method");
+
+                result = method.Invoke(null, new object[] { this, context }) as TripleEContext;
+            }
+
+            return result?.Serialize() ?? Array.Empty<byte>();
+        }
+
+        private TripleEContext ParseBuffer(byte[] buffer)
+        {
+            using var stream = new MemoryStream(buffer);
+            using var reader = new StreamReader(stream);
+
+            var preamble = reader.ReadLine().Split(' ');
+
+            if (preamble.Length != 2 || preamble[0] != "3EPROTO")
+            {
+                return null;
+            }
+
+            TripleEContext context = new()
+            {
+                Method = Enum.Parse<TripleEMethods>(preamble[1]),
+                Headers = new()
+            };
+
+            while (true)
+            {
+                var line = reader.ReadLine()?.Trim();
+
+                if (string.IsNullOrEmpty(line))
+                {
+                    break;
+                }
+
+                var firstColonPos = line.IndexOf(':');
+                var valueStartPos = firstColonPos + 1;
+
+                if (firstColonPos == -1)
+                    break;
+
+                var key = line[0..firstColonPos].Trim('\0', '\n', ' ');
+                var value = line[valueStartPos..].Trim('\0', '\n', ' ');
+
+                context.Headers.Add(key, value);
+            }
+
+            context.Body = reader.ReadToEnd()?.Trim();
+
+            return context;
+        }
+
+        public void RelayRequest(InternalSocketContext to, TripleEContext context)
+        {
+            if (!context.Headers.ContainsKey("Timestamp"))
+                context.Headers.Add("Timestamp", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+
+            var data = context.Serialize();
+            to.AcceptedSocket.SendAsync(data, SocketFlags.None);
         }
 
         private byte[] InternalSocketReceived(InternalSocketContext primitiveContext)
@@ -63,47 +150,25 @@ namespace AssignmentServer.E2EECS.Server
 
             try
             {
-                using var stream = new MemoryStream(primitiveContext.Buffer);
-                using var reader = new StreamReader(stream);
+                var context = ParseBuffer(primitiveContext.Buffer);
 
-                var preamble = reader.ReadLine().Split(' ');
-
-                if (preamble.Length != 2 || preamble[0] != "3EPROTO")
-                    throw new Exception("Invalid packet send");
-
-                TripleEContext context = new()
+                if (context is null) 
                 {
-                    Method = Enum.Parse<TripleEMethods>(preamble[1]),
-                    Headers = new ()
-                };
-
-                while (true) 
-                {
-                    var line = reader.ReadLine()?.Trim();
-                    
-                    if (string.IsNullOrEmpty(line))
-                    {
-                        break;
-                    }
-
-                    var firstColonPos = line.IndexOf(':');
-                    var valueStartPos = firstColonPos + 1;
-
-                    if (firstColonPos == -1)
-                        break;
-
-                    var key = line[0..firstColonPos];
-                    var value = line[valueStartPos..];
-
-                    context.Headers.Add(key, value);
+                    Console.WriteLine("[ERROR] Failed to parse buffer into context");
+                    return Array.Empty<byte>();
                 }
 
-                context.Body = reader.ReadToEnd();
+                context.SocketContext = primitiveContext;
 
                 return DispatchMethod(context);
             }
             catch (Exception ex)
             {
+                if (ex.InnerException is not null)
+                    ex = ex.InnerException;
+
+                Console.WriteLine("[ERROR] {0}", ex.Message);
+
                 primitiveContext.Invalid = true;
                 return TripleEContext.ErrorContext(ex.Message).Serialize();
             }
